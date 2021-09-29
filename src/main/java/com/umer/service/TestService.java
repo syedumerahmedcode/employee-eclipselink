36138aa6b86d923e0291fee6589ef1e654888a47
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-09-29 09:54:24 +0200

    Feature: Added a test service which calls employee JPA Entity via a main method.

--- src/main/java/com/umer/service/TestService.java
+++ src/main/java/com/umer/service/TestService.java
@@ -1,8 +1,41 @@
 package com.umer.service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.umer.entity.Employee;
+
 public class TestService {
 
-	public static void main(String[] args) {
+	private static final String PERSISTENCE_UNIT_NAME = "employee";
+
+	public static void main(String[] args) throws Exception {
+		EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		EntityManager entityManager=entityManagerFactory.createEntityManager(); 
 		
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		Employee employee = createEmployeeToStoreInTheDatabase();
+		
+		entityManager.persist(employee);
+		
+		entityTransaction.commit();
+		System.out.println("Employee added successfully");
+		entityManager.close();
+	}
+
+	private static Employee createEmployeeToStoreInTheDatabase() {
+		Employee employee=new Employee();
+		employee.setId(2);
+		employee.setFirst_name("firstName");
+		employee.setLast_name("lastName");
+		employee.setUsername("username");
+		employee.setPassword("password");
+		employee.setAddress("address");
+		employee.setContact("contact");
+		return employee;
 	}
 }
